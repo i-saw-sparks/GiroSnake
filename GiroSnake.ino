ce6111e19c0b2fb07169c48bea9cb790ffc1c0fe
i-saw-sparks <andresmolinaceti@gmail.com>
2019-05-02 21:09:18 -0500

    Comentarios para preguntas harcors

--- GiroSnake.ino
+++ GiroSnake.ino
@@ -10,7 +10,7 @@
 #define TFT_RST 8
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
+//Declaración del objeto del acelerometro, pasa por parametro wire y la dirección que se le asignará al dispositivo
 MPU9250 gyro(Wire, 0x68);
 float x, y;
 float pixX =20, pixY=20;
@@ -19,7 +19,7 @@ int lastMov = 4;
 int puntos = 9;
 bool fin = false, inicio = true, once = true, punt = true;
  
-
+//Clase de los cuadritos que se van renderizando para crear a la serpiente
 class SQsnake{
   public: 
   int posX, posY;
@@ -41,7 +41,7 @@ class SQsnake{
     tft.drawRect(posX, posY, 2, 2, ST77XX_WHITE);
   }
 };
-
+//Clase de las frutitas que hereda de la clase SQsnake
 class Fruta: public SQsnake{
   public:
   
@@ -56,7 +56,8 @@ class Fruta: public SQsnake{
   }
   
 };
-
+//Listas dinámicas donde se guardan todas las posiciones de todos los 
+//cuadros de la serpiente y de las frutas
 List<int> listaX;
 List<int> listaY;
 List<int> frutaX;
@@ -67,7 +68,7 @@ int puntuacion[6];
 
 Fruta fruta(30, 30);
 void setup() {
-  gyro.begin();
+  gyro.begin();//Se inicia el giroscopio
   Wire.begin();
   tft.initR(INITR_BLACKTAB);
   tft.setTextColor(ST77XX_BLACK);
@@ -79,63 +80,64 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
-  if(!fin){
+  if(!fin){ // la variable fin controla el bloque que se ejecutará solo cuando el juego esté en partida
 
   if(inicio){    
-    frutaX.Add(30);
+    frutaX.Add(30); //Se añade la fruta inicial
     frutaY.Add(30);
     fruta.Draw();
     inicio = false;
   }
   
   if(gyro.readSensor() > 0){
-      x = gyro.getAccelX_mss();
+      x = gyro.getAccelX_mss(); //Se lee el eje X y Y de aceleración del acelerometro
       y = gyro.getAccelY_mss();
   }
 
-  Move(x, y);
+  Move(x, y); //Move asigna las posiciones donde se renderizará el nuevo cuadro de la serpiente
 
-  for(int i=0; i < listaX.Count(); i++){
+  for(int i=0; i < listaX.Count(); i++){ //Se verifica si no hay colisión con la propia serpiente
     if(pixX == listaX[i] && pixY==listaY[i]){
       fin = true;
     }    
   }
 
-  for(int i = 0; i < frutaX.Count(); i++){
-    if(pixX >= frutaX[0]-1 && pixX <= frutaX[0]+3 && pixY >= frutaY[0]-1 && pixY <= frutaY[0]+ 3){
+
+  for(int i = 0; i < frutaX.Count(); i++){ 
+    if(pixX >= frutaX[0]-1 && pixX <= frutaX[0]+3 && pixY >= frutaY[0]-1 && pixY <= frutaY[0]+ 3){ // Se verifica si hubo colosión con las frutas
         puntos += 3;
-        frutaX.RemoveFirst();
+        frutaX.RemoveFirst(); 
         frutaY.RemoveFirst();
         fruta.Destroy(fruta.posX, fruta.posY);
         int tempx, tempy;
         tempx= random(0, 126);
-        tempy = random(0, 130);        
+        tempy = random(0, 130);       
         fruta.SetPos(tempx, tempy);
         fruta.Draw();
         frutaX.Add(tempx);
-        frutaY.Add(tempy);
+        frutaY.Add(tempy); //Se crea una nueva fruta y se añade a la lista dinámica
         tft.fillRect(100, 140, 30, 30, ST77XX_WHITE);
         tft.setCursor(100, 140);
         tft.print(puntos/3);
   }
-  }    
-  
-  
-  SQsnake snake((int)pixX, (int)pixY);
-  
+  } 
+
+     
+  SQsnake snake((int)pixX, (int)pixY); //Se crea nuevo cuadro de la serpiente
+    
   listaX.Add((int)pixX);
   listaY.Add((int)pixY);
-  if(listaX.Count()>puntos){
+  if(listaX.Count()>puntos){ //Si hay más cuadros de serpiente que puntos, se destruyen los últimos cuadros
     snake.Destroy(listaX[0], listaY[0]); 
     listaX.RemoveFirst();
     listaY.RemoveFirst();
   }
   snake.Draw();
   delay(50);
-  }
-  if(fin){    
+  }//Aqui termina el bloque de codigo de la partida
+  
+  if(fin){ //Aqui se entra cuando la partida se termina
       if(once){
         tft.fillScreen(ST77XX_WHITE);
         tft.setCursor(6,0);
@@ -144,16 +146,16 @@ void loop() {
         tft.setCursor(50,15);
         
         once = false;
-        i2c_eeprom_write_byte(0x50, 0x05, (byte)(puntos/3));
+        i2c_eeprom_write_byte(0x50, 0x05, (byte)(puntos/3));//Se escribe tu puntaje en la EEPROM
         delay(100);
       }
 
       for(int i = 0; i<6; i++){
-        puntuacion[i] = i2c_eeprom_read_byte(0x50, i);
+        puntuacion[i] = i2c_eeprom_read_byte(0x50, i); //Se leen los valores de la EEPROM
         delay(100);
       }
 
-      for(int i = 0; i<6; i++){
+      for(int i = 0; i<6; i++){ //Algoritmo de burbuja
         for(int j=0; j<6; j++){
           if(puntuacion[i]>puntuacion[j]){
             int temp = puntuacion[j];
@@ -172,7 +174,7 @@ void loop() {
         tft.setCursor(50,y);
         y+=15;
         tft.println(puntuacion[i]);
-        i2c_eeprom_write_byte(0x50, i, (byte)puntuacion[i]);
+        i2c_eeprom_write_byte(0x50, i, (byte)puntuacion[i]); //Se muestran los resultados y se escriben en la EEPROM en orden
         delay(100);
       }
 
@@ -182,7 +184,7 @@ void loop() {
 }
   
 
-void Move(float x, float y){
+void Move(float x, float y){ //Move determina la posición de renderizado del próximo cuadro de serpiente
   if(x > 2.5 && lastMov != 2){
     pixX-=1;
     lastMov = 1;   
@@ -231,23 +233,23 @@ void Move(float x, float y){
 }
 
 
-void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data ) {
+void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data ) { //funcion para escribit un byte en la EEPROM
     int rdata = data;
-    Wire.beginTransmission(deviceaddress);
-    Wire.write((int)(eeaddress >> 8)); // MSB
-    Wire.write((int)(eeaddress & 0xFF)); // LSB
-    Wire.write(rdata);
-    Wire.endTransmission();
+    Wire.beginTransmission(deviceaddress); //Se inicio la transmición de datos
+    Wire.write((int)(eeaddress >> 8)); // se determina el bit mas significativo mediante un right shift
+    Wire.write((int)(eeaddress & 0xFF)); // la mascara es usada para solo tomar en cuenta los primeros 8 bits
+    Wire.write(rdata); //Se escribe el byte en la memoria
+    Wire.endTransmission(); //Se termina la transmición
 }
 
 
 byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress ) {
     byte rdata = 0xFF;
     Wire.beginTransmission(deviceaddress);
-    Wire.write((int)(eeaddress >> 8)); // MSB
-    Wire.write((int)(eeaddress & 0xFF)); // LSB
+    Wire.write((int)(eeaddress >> 8)); 
+    Wire.write((int)(eeaddress & 0xFF)); 
     Wire.endTransmission();
-    Wire.requestFrom(deviceaddress,1);
+    Wire.requestFrom(deviceaddress,1); 
     if (Wire.available()) rdata = Wire.read();
     return rdata;
 }
